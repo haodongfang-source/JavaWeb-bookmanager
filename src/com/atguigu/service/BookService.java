55c359b9b01af132be446631d7dc60e5bd1439e7
haodongfang-source <dongfang7212@gmail.com>
2021-12-15 14:52:38 +0800

    before page divide

--- src/com/atguigu/service/BookService.java
+++ src/com/atguigu/service/BookService.java
@@ -1,10 +1,12 @@
 package com.atguigu.service;
 
 import com.atguigu.pojo.Book;
+import com.atguigu.pojo.Page;
 
 import java.util.List;
 
 public interface BookService {
+
 
     public void addBook(Book book);
 
@@ -15,5 +17,6 @@ public interface BookService {
     public Book queryBookById(Integer id);
 
     public List<Book> queryBooks();
+
 
 }
